9935e72b9b0264209b74611a9861cad60284e5de
Zin <62830952+Zintixx@users.noreply.github.com>
2025-06-24 10:44:12 -0700

    Updated Converter

--- WeblateConverter/Enum/Locale.cs
+++ WeblateConverter/Enum/Locale.cs
@@ -1,8 +1,10 @@
-﻿namespace WeblateConverter.Enum;
+﻿﻿﻿namespace WeblateConverter.Enum;
 
 public enum Locale {
     en,
     kr,
+    kr_latest,  // Latest Korean XML strings
+    ko,  // Korean output locale for JSON
     de,
     pt,
     cn,
